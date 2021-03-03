8e65e6ec5a6dee2f90bc7bf15677f5123e7935c9
Sanyam200 <sanyamdawra14@gmail.com>
2021-03-03 21:42:59 +0530

    Authentication Django

---
+++ userproject/home/urls.py
@@ -0,0 +1,8 @@
+from home import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.index, name="home"),
+    path('login', views.loginUser, name="login"),
+    path('logout', views.logoutUser, name="logout")    
+]
